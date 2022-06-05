3b8d8f38a9627badb567c16ee249e5aa512876d8
eskokado <eskokado@gmail.com>
2022-06-04 23:31:47 -0300

    feat: calback em funções

---
+++ exemplos/callback_funcoes.ts
@@ -0,0 +1,12 @@
+function aoQuadrado(valor: number) {
+    return valor ** 2;
+}
+
+function somarETratar(valor1: number, valor2: number, callback: (valor: number) => number): number {
+  let soma = valor1 + valor2;
+  return callback(soma);
+}
+
+let result = somarETratar(2, 3, aoQuadrado);
+
+console.log(`Restultado: ${result}`);
