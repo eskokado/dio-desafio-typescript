d92ef83fdae12c51eb413804cfdb61aef7861c92
eskokado <eskokado@gmail.com>
2022-06-04 21:43:44 -0300

    feat: deve printar frase

--- app.ts
+++ app.ts
@@ -2,14 +2,21 @@ let button = document.getElementById("button");
 let input1 = document.getElementById("input1") as HTMLInputElement;
 let input2 = document.getElementById("input2") as HTMLInputElement;
 
-function somarNumeros(numero1: number, numero2: number) {
-  return numero1 + numero2;
+function somarNumeros(numero1: number, numero2: number, devePrintar: boolean, frase: string) {
+  let resultado = numero1 + numero2;
+  if (devePrintar) {
+    console.log(`${frase} ${resultado}`);
+  }
+  return resultado;
 }
+
+let devePrintar = true;
+let frase = "O valor é: ";
 
 if (button) {
   button.addEventListener('click', function () {
     if (input1 && input2){
-      console.log(somarNumeros(Number(input1.value), Number(input2.value)));
+      somarNumeros(Number(input1.value), Number(input2.value), devePrintar, frase);
     }
   });
 }
